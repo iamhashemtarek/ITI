39826c2ca3dcb80173cfe55efc9846146c8682be
iamhashemtarek <hashemtarek2012@gmail.com>
2024-12-15 14:49:56 +0200

    some comments

--- CSharp/Labs/task-7/Program.cs
+++ CSharp/Labs/task-7/Program.cs
@@ -6,12 +6,19 @@
         {
             Book myBook = new Book("b-ISBN", "b-Title", ["author-1", "author-2"], new DateTime(2023, 1, 1), 200);
             //Console.WriteLine( myBook.ToString());
-            BookFunctionsDelegate<Book, string> d = delegate (Book book)
-            {
-                return $"price: {book.Price}";
-            };
+            //BookFunctionsDelegate<Book, string> d = delegate (Book book)
+            //{
+            //    return $"price: {book.Price}";
+            //};
 
-            BookFunctionsDelegate<Book, string> d1 =  book => $"price: {book.Price}";
+            //BookFunctionsDelegate<Book, string> d1 =  book => $"price: {book.Price}";
+
+            //LibraryEngine.ProcessBooks(/*list*/, delegate (Book book)
+            //{
+            //    return $"price: {book.Price}";
+            //});
+
+            //LibraryEngine.ProcessBooks(/*list*/, book => $"price: {book.Price}");
 
         }
     }

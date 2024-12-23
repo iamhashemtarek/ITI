feb7a4c56d7384316b4ae49a477e436213bcc67e
iamhashemtarek <hashemtarek2012@gmail.com>
2024-12-23 20:13:40 +0200

    database day05 tasks - advanced

---
+++ database/labs/day05/SQLQuery2.sql
@@ -0,0 +1,102 @@
+﻿--Part-1: Use ITI DB
+use ITI;
+
+--⦁	Retrieve number of students who have a value in their age. 
+select count(st_age)
+from Student;
+
+--⦁	Get all instructors Names without repetition
+select distinct ins_name
+from Instructor
+
+--⦁	Display student with the following Format (use isNull function)
+		--Student ID	Student Full Name	Department name
+select St_Id, ISNULL(St_Fname, '') + ' ' + ISNULL(St_Lname, '') as fullname
+from Student
+
+--⦁	Display instructor Name and Department Name 
+select i.Ins_Name, d.Dept_Name
+from Instructor as i
+join Department as d on i.Dept_Id = d.Dept_Id
+
+--Note: display all the instructors if they are attached to a department or not
+select i.Ins_Name, ISNULL(d.Dept_name, 'N/A')
+from Instructor as i
+left join Department as d on i.Dept_Id = d.Dept_Id
+
+--⦁	Display student full name and the name of the course he is taking --For only courses which have a grade  
+select s.St_Fname + ' ' + s.St_Lname  as fullname, c.Crs_Name
+from Student as s
+join Stud_Course as sc on s.St_Id = sc.St_Id
+join Course as c on sc.Crs_Id = c.Crs_Id
+where sc.Grade is not null
+
+
+--⦁	Display number of courses for each topic name
+select t.Top_Name, COUNT(c.Crs_Id) as number_of_courses
+from Course as c
+join Topic as t on c.Top_Id = t.Top_Id
+group by t.Top_Name
+
+--⦁	Display max and min salary for instructors
+select 'max salary' as salary,  MAX(Salary)
+from Instructor
+union 
+select 'min salary' as salary, MIN(salary)
+from Instructor
+
+select MAX(salary) as max_salary, min(salary) as min_salary
+from Instructor
+
+--⦁	Display instructors who have salaries less than the average salary of all instructors.
+select Ins_Name, Salary
+from Instructor
+where Salary < (select AVG(salary) from Instructor)
+
+--⦁	Display the Department name that contains the instructor who receives the minimum salary.
+select i.Ins_Name, i.Salary, d.Dept_Name
+from Instructor as i 
+join Department as d on i.Dept_Id = d.Dept_Id
+where i.Salary = (select min(salary) from Instructor)
+
+--⦁	 Select max two salaries in instructor table. 
+with rankedSalaries as (
+	select Salary, DENSE_RANK() over (order by salary desc)  as DR
+	from Instructor
+)
+select distinct salary 
+from rankedSalaries
+where DR <=2
+order by salary desc
+
+--⦁	 Select instructor name and his salary but if there is no salary display instructor bonus keyword. “use coalesce Function”
+select Ins_Name, coalesce(Salary,  'bonus') as salary
+from Instructor
+
+--⦁	Select Average Salary for instructors 
+select AVG(salary) as average_salary
+from Instructor
+
+--⦁	Select Student first name and the data of his supervisor 
+select s.St_Fname, sv.*
+from Student as s
+join Student as sv on s.St_super = sv.St_Id
+
+--⦁	Write a query to select the highest two salaries in Each Department for instructors who have salaries. “using one of Ranking Functions”
+with rankedSalaries as (
+	select Dept_id, Salary, DENSE_RANK() over (partition by Dept_id order by salary desc) as DR
+	from Instructor 
+	where salary is not null
+)
+select  Dept_id, salary 
+from rankedSalaries
+where DR <= 2
+
+--⦁	 Write a query to select a random student from each department.  “using one of Ranking Functions”
+with randomStudent as (
+	select Dept_id, ISNULL(St_Fname, '') + ' '+ ISNULL(St_Lname,'')  as fullname, ROW_NUMBER() over (partition by Dept_Id order by NEWID()) as RN
+	from Student
+)
+select Dept_Id, fullname
+from randomStudent
+where RN = 1;
---
+++ database/labs/day05/SQLQuery3.sql
@@ -0,0 +1,82 @@
+﻿--Part-2: Use AdventureWorks DB
+use AdventureWorks2012;
+
+--⦁	Display the SalesOrderID, ShipDate of the SalesOrderHeader table
+--(Sales schema) to show SalesOrders that occurred within the period ‘7/28/2002’ and ‘7/29/2014’
+select SalesOrderID, ShipDate
+from Sales.SalesOrderHeader
+where ShipDate between '7/28/2002' and '7/29/2014'
+
+--⦁	Display only Products(Production schema) with a StandardCost below $110.00 (show ProductID, Name only)
+select ProductID, Name, StandardCost
+from Production.Product
+where StandardCost < 110.00
+
+--⦁	Display ProductID, Name if its weight is unknown
+select ProductID, Name
+from Production.Product
+where Weight is not null
+
+--⦁	 Display all Products with a Silver, Black, or Red Color
+select *
+from Production.Product
+where Color in  ('Silver', 'Black', 'Red')
+
+--⦁	 Display any Product with a Name starting with the letter B
+select Name
+from Production.Product
+where LEFT(Name, 1) = 'B'
+
+--⦁	Run the following Query
+UPDATE Production.ProductDescription
+SET Description = 'Chromoly steel_High of defects'
+WHERE ProductDescriptionID = 3
+--Then write a query that displays any Product description with underscore value in its description.
+select Description 
+from Production.ProductDescription
+where CHARINDEX('_', Description, 1) > 0
+
+--⦁	Calculate sum of TotalDue for each OrderDate in Sales.SalesOrderHeader table for the period between  '7/1/2001' and '7/31/2014'
+select sum(TotalDue)
+from Sales.SalesOrderHeader
+where OrderDate between '7/1/2001' and '7/31/2014'
+group by OrderDate
+
+--⦁	 Display the Employees HireDate (note no repeated values are allowed)
+select distinct HireDate
+from HumanResources.Employee
+
+--⦁	 Calculate the average of the unique ListPrices in the Product table
+select AVG(distinct ListPrice)
+from Production.Product
+
+--⦁	Display the Product Name and its ListPrice within the values of 100 and 120
+--the list should has the following format "The [product name] is only! [List price]" 
+--(the list will be sorted according to its ListPrice value)
+select 'The ' + Name + ' os only! ' + cast(ListPrice as varchar)
+from Production.Product
+where ListPrice between 100 and 120
+	
+
+--⦁	 Transfer the rowguid ,Name, SalesPersonID, Demographics from Sales.Store table  in a newly created table named [store_Archive]
+--Note: Check your database to see the new table and how many rows in it?
+select rowguid ,Name, SalesPersonID, Demographics 
+into store_archive
+from Sales.Store
+
+--⦁	Try the previous query but without transferring the data? 
+select rowguid ,Name, SalesPersonID, Demographics 
+into store_archive
+from Sales.Store
+where 1 = 2;
+
+--⦁	Using union statement, retrieve the today’s date in different styles using convert or format funtion. 
+SELECT convert(varchar, getdate(), 1) AS DateStyle1 -- MM/DD/YYYY
+UNION
+SELECT convert(varchar, getdate(), 3) AS DateStyle2 -- DD/MM/YYYY
+UNION
+SELECT convert(varchar, getdate(), 4) AS DateStyle3 -- DD.MM.YY
+UNION
+SELECT format(getdate(), 'yyyy-MM-dd') AS DateStyle4 -- ISO 8601 format (yyyy-MM-dd)
+UNION
+SELECT format(getdate(), 'dddd, MMMM dd, yyyy') AS DateStyle5 -- Full date with day and month name

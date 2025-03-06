a2cbe91fe237572daa1895466b7f50015c6d2932
iamhashemtarek <hashemtarek2012@gmail.com>
2025-03-06 03:44:14 +0200

    add unit tests for Calculator class with xUnit assertions

---
+++ Testing/Testing/Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator;
+namespace Calculator.Tests
+{
+    public class CalculatorTests
+    {
+        private readonly Calculator _calculator = new();
+
+        [Fact]
+        public void IsEven_ShouldReturnTrueForEvenNumbers()
+        {
+            Assert.True(_calculator.IsEven(2));
+            Assert.False(_calculator.IsEven(3));
+        }
+
+        [Fact]
+        public void Greet_ShouldReturnGreetingMessage()
+        {
+            Assert.Equal("Hello, hashem!", _calculator.Greet("hashem"));
+            Assert.Contains("hashem", _calculator.Greet("hashem"));
+            Assert.StartsWith("Hello", _calculator.Greet("hashem"));
+        }
+
+        [Fact]
+        public void GetEvenNumbers_ShouldReturnAllEvenNumbersUpToMax()
+        {
+            Assert.Equal([2, 4], _calculator.GetEvenNumbers(5));
+            Assert.Empty(_calculator.GetEvenNumbers(0));
+            Assert.All(
+                _calculator.GetEvenNumbers(5),
+                item => Assert.True(item % 2 == 0)
+                );
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(2, 4, 6)]
+        public void Add_ShouldReturnSumOfTwoNumbers(int x, int y, int expected)
+        {
+            Assert.Equal(expected, _calculator.Add(x, y));
+
+        }
+
+        [Fact]
+        public void GetResultAsObject_ShouldReturnCorrectTypes()
+        {
+            Assert.IsType<int>(_calculator.GetResultAsObject(true));
+            Assert.IsType<string>(_calculator.GetResultAsObject(false));
+        }
+        [Fact]
+        public void GetReference_ShouldReturnSameOrNullReference()
+        {
+            Assert.Null(_calculator.GetReference(true));
+            Assert.Same(_calculator, _calculator.GetReference(false));
+        }
+    }
+}
---
+++ Testing/Testing/calculator/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public bool IsEven(int number) => number % 2 == 0;
+
+        public string Greet(string name) => $"Hello, {name}!";
+
+        public int[] GetEvenNumbers(int max)
+        {
+            var evens = new List<int>();
+            for (int i = 1; i <= max; i++)
+            {
+                if (i % 2 == 0) evens.Add(i);
+            }
+            return evens.ToArray();
+        }
+
+        public int Add(int a, int b) => a + b;
+
+        public object GetResultAsObject(bool returnNumber)
+        {
+            return returnNumber ? 42 : "Not a number";
+        }
+
+        public object GetReference(bool returnNull)
+        {
+            return returnNull ? null : this;
+        }
+
+
+    }
+}
